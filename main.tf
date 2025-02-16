cdb5f10c5240a07ed45ed48f03096ebd593bebdb
Sahil Nagpal <sahiii201190@gmail.com>
2025-02-15 21:29:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "google_storage_bucket" "my_bucket1" {
   location                        = "us-central1" # Replace with your desired region
-  name                            = "unique-bucket-name-${random_id.bucket_prefix.hex}" # Bucket names must be globally unique
+  name                            = "bucket1" # Bucket names must be globally unique
   project                         = "red-abstraction-446113-u1" # Your Project ID
-  force_destroy                   = true # Use with caution in production!
+  force_destroy                   = false # Use with caution in production!
   uniform_bucket_level_access = true # Recommended for security
   public_access_prevention      = "enforced" # Correct syntax
 
