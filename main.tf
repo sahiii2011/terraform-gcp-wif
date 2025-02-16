95fcb0e335b0b7385a75a39f66e9bbd48b9e120f
Sahil Nagpal <sahiii201190@gmail.com>
2025-02-16 12:42:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,13 +6,4 @@ resource "google_storage_bucket" "my_bucket1" {
   uniform_bucket_level_access = true # Recommended for security
   public_access_prevention      = "enforced" # Correct syntax
 }
-  # Optional settings (uncomment and customize as needed)
-  storage_class = "STANDARD" # Or "NEARLINE", "COLDLINE", "ARCHIVE"
-  versioning {
-    enabled = true
-  }
   
-resource "random_id" "bucket_prefix" {
- byte_length = 8 # Generates a random hex string
-}
- 
